aaf8bc23655ca2ebdadb95cdcc292371e9491543
starfetch <80041179+starfetch@users.noreply.github.com>
2021-11-07 15:07:26 +0100

    feat: move closure end closer to waist/hips

--- packages/bucky/src/closure.js
+++ packages/bucky/src/closure.js
@@ -20,10 +20,11 @@ export default function (part) {
     points.test = points.hps.shift(-90,20)
     points.cbNeckHem = points.cfSeat.shift(-90,points.seat.dy(points.hem))
     points.closureHem = points.cbNeckHem.shift(0,points.cfNeck.dx(points.hps)/2)
+    points.closureWaist = points.closureHem.shift(90,points.waist.dy(points.hem)*0.8)
 
     paths.side = new Path()
 	.move(points.hps)
-	.line(points.closureHem)
+	.line(points.closureWaist)
     
     
   if (complete) {
